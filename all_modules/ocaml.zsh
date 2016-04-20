65c14399882d945e90aef6f55c230fae50b35ce5
Cooper LeBrun <cooperlebrun@gmail.com>
2016-04-20 15:13:17 -0700

    add utop aliases

--- all_modules/ocaml.zsh
+++ all_modules/ocaml.zsh
@@ -1,2 +1,7 @@
 # OPAM configuration
 . /Users/cooperlebrun/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
+alias save-utop="cat ~/.utop-history >> ~/Projects/OCaml/history.log"
+
+# Bad idea?
+# auto save utop history with above command after finished with session.
+alias -g utop-exp="utop ; save-utop"
