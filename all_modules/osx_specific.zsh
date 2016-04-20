23015b34abaf0c8485f8a44d0a8bcbbb68f0fcce
Cooper LeBrun <cooperlebrun@gmail.com>
2016-04-20 15:13:56 -0700

    add full update/upgrade alias for brew

--- all_modules/osx_specific.zsh
+++ all_modules/osx_specific.zsh
@@ -6,4 +6,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   if which psql > /dev/null; then
     alias postgres-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
   fi
+  which brew > /dev/null && alias buu='brew update && brew upgrade'
+  alias -g wh="say -v whisper"
 fi
